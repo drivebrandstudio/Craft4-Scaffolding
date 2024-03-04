1b3c73100e5d4210d64b416a08d34d0128b95d46
Dan <danielcrump@outlook.com>
2024-03-04 11:40:41 -0500

    working graphql ping request and render

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '../js/app'
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "../js/app";
 
-ReactDOM.createRoot(document.querySelector('#root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+ReactDOM.createRoot(document.querySelector("#root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // Here is where SSR would gain us value. Pull in JS libraries easily, utilize them, have the Server
 // return HTML to quickly render while we are fetching data to hydrate said HTML.
 
-
 function App() {
-    return (
-        <h1>App</h1>
-    );
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const req = fetch("https://craft4-scaffolding.ddev.site/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: "{ping}",
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+      });
+  }, []);
+
+  return <h1>{`${data?.data?.ping}`}</h1>;
 }
