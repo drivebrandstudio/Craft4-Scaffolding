1a885596718a57af723b6372b4a3d496e101c108
Dan <danielcrump@outlook.com>
2024-04-19 13:27:41 -0400

    remove animatepresence

--- src/react/components/ToolTip.tsx
+++ src/react/components/ToolTip.tsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import {
   motion,
   useTransform,
-  AnimatePresence,
   useMotionValue,
   useSpring,
 } from "framer-motion";
@@ -45,7 +44,6 @@ export const AnimatedTooltip = ({
           onMouseEnter={() => setHoveredIndex(item.id)}
           onMouseLeave={() => setHoveredIndex(null)}
         >
-          <AnimatePresence mode="wait">
             {hoveredIndex === item.id && (
               <motion.div
                 initial={{ opacity: 0, y: 20, scale: 0.6 }}
@@ -75,7 +73,6 @@ export const AnimatedTooltip = ({
                 <div className="text-white text-xs">{item.jobTitle}</div>
               </motion.div>
             )}
-          </AnimatePresence>
           {!!item.image.length &&
           <img
             onMouseMove={handleMouseMove}
